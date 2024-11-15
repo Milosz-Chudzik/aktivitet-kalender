f6f8c8661c789b90ef8aec10ba63d796826dfcc9
Milosz <miloszchudzik07@gmail.com>
2024-11-15 11:14:44 +0100

    Update kalender.py

--- kalender.py
+++ kalender.py
@@ -50,8 +50,286 @@ def vis_januar_2024():
 def vis_februar_2024():
     text_calendar = calendar.TextCalendar()
     text_calendar.prmonth(2024, 2)
+    if input("vil du skrive ett notat ja/nei: ").lower() == "ja":
+        dato = valgtDato()
+        # if dato not in range(1, 1000000000000):  
+        #     print("Ugyldig dato!")
+        
+        # else: 
+        notat = input("Skriv notat: ")
+        sql = f"""
+        UPDATE february
+        SET notes = '{notat}'
+        WHERE date = '{dato}'
+        """
+        mycursor.execute(sql)
+        mydb.commit()
+        print(f"Notat lagret for {dato}.")
+            
 
 
+    else:
+        if input("vil du gå tilbake ja/nei") == "ja":
+            start()
+
+
+def vis_mars_2024():
+    text_calendar = calendar.TextCalendar()
+    text_calendar.prmonth(2024, 3)
+    if input("vil du skrive ett notat ja/nei: ").lower() == "ja":
+        dato = valgtDato()
+        # if dato not in range(1, 1000000000000):  
+        #     print("Ugyldig dato!")
+        
+        # else: 
+        notat = input("Skriv notat: ")
+        sql = f"""
+        UPDATE march
+        SET notes = '{notat}'
+        WHERE date = '{dato}'
+        """
+        mycursor.execute(sql)
+        mydb.commit()
+        print(f"Notat lagret for {dato}.")
+            
+
+
+    else:
+        if input("vil du gå tilbake ja/nei") == "ja":
+            start()
+
+
+def vis_april_2024():
+    text_calendar = calendar.TextCalendar()
+    text_calendar.prmonth(2024, 4)
+    if input("vil du skrive ett notat ja/nei: ").lower() == "ja":
+        dato = valgtDato()
+        # if dato not in range(1, 1000000000000):  
+        #     print("Ugyldig dato!")
+        
+        # else: 
+        notat = input("Skriv notat: ")
+        sql = f"""
+        UPDATE april
+        SET notes = '{notat}'
+        WHERE date = '{dato}'
+        """
+        mycursor.execute(sql)
+        mydb.commit()
+        print(f"Notat lagret for {dato}.")
+            
+
+
+    else:
+        if input("vil du gå tilbake ja/nei") == "ja":
+            start()
+
+
+def vis_mai_2024():
+    text_calendar = calendar.TextCalendar()
+    text_calendar.prmonth(2024, 5)
+    if input("vil du skrive ett notat ja/nei: ").lower() == "ja":
+        dato = valgtDato()
+        # if dato not in range(1, 1000000000000):  
+        #     print("Ugyldig dato!")
+        
+        # else: 
+        notat = input("Skriv notat: ")
+        sql = f"""
+        UPDATE may
+        SET notes = '{notat}'
+        WHERE date = '{dato}'
+        """
+        mycursor.execute(sql)
+        mydb.commit()
+        print(f"Notat lagret for {dato}.")
+            
+
+
+    else:
+        if input("vil du gå tilbake ja/nei") == "ja":
+            start()
+
+
+def vis_juni_2024():
+    text_calendar = calendar.TextCalendar()
+    text_calendar.prmonth(2024, 6)
+    if input("vil du skrive ett notat ja/nei: ").lower() == "ja":
+        dato = valgtDato()
+        # if dato not in range(1, 1000000000000):  
+        #     print("Ugyldig dato!")
+        
+        # else: 
+        notat = input("Skriv notat: ")
+        sql = f"""
+        UPDATE june
+        SET notes = '{notat}'
+        WHERE date = '{dato}'
+        """
+        mycursor.execute(sql)
+        mydb.commit()
+        print(f"Notat lagret for {dato}.")
+            
+
+
+    else:
+        if input("vil du gå tilbake ja/nei") == "ja":
+            start()
+
+
+def vis_juli_2024():
+    text_calendar = calendar.TextCalendar()
+    text_calendar.prmonth(2024, 7)
+    if input("vil du skrive ett notat ja/nei: ").lower() == "ja":
+        dato = valgtDato()
+        # if dato not in range(1, 1000000000000):  
+        #     print("Ugyldig dato!")
+        
+        # else: 
+        notat = input("Skriv notat: ")
+        sql = f"""
+        UPDATE july
+        SET notes = '{notat}'
+        WHERE date = '{dato}'
+        """
+        mycursor.execute(sql)
+        mydb.commit()
+        print(f"Notat lagret for {dato}.")
+            
+
+
+    else:
+        if input("vil du gå tilbake ja/nei") == "ja":
+            start()
+
+
+def vis_august_2024():
+    text_calendar = calendar.TextCalendar()
+    text_calendar.prmonth(2024, 8)
+    if input("vil du skrive ett notat ja/nei: ").lower() == "ja":
+        dato = valgtDato()
+        # if dato not in range(1, 1000000000000):  
+        #     print("Ugyldig dato!")
+        
+        # else: 
+        notat = input("Skriv notat: ")
+        sql = f"""
+        UPDATE august
+        SET notes = '{notat}'
+        WHERE date = '{dato}'
+        """
+        mycursor.execute(sql)
+        mydb.commit()
+        print(f"Notat lagret for {dato}.")
+            
+
+
+    else:
+        if input("vil du gå tilbake ja/nei") == "ja":
+            start()
+
+
+def vis_september_2024():
+    text_calendar = calendar.TextCalendar()
+    text_calendar.prmonth(2024, 9)
+    if input("vil du skrive ett notat ja/nei: ").lower() == "ja":
+        dato = valgtDato()
+        # if dato not in range(1, 1000000000000):  
+        #     print("Ugyldig dato!")
+        
+        # else: 
+        notat = input("Skriv notat: ")
+        sql = f"""
+        UPDATE september
+        SET notes = '{notat}'
+        WHERE date = '{dato}'
+        """
+        mycursor.execute(sql)
+        mydb.commit()
+        print(f"Notat lagret for {dato}.")
+            
+
+
+    else:
+        if input("vil du gå tilbake ja/nei") == "ja":
+            start()
+
+
+def vis_oktober_2024():
+    text_calendar = calendar.TextCalendar()
+    text_calendar.prmonth(2024, 10)
+    if input("vil du skrive ett notat ja/nei: ").lower() == "ja":
+        dato = valgtDato()
+        # if dato not in range(1, 1000000000000):  
+        #     print("Ugyldig dato!")
+        
+        # else: 
+        notat = input("Skriv notat: ")
+        sql = f"""
+        UPDATE october
+        SET notes = '{notat}'
+        WHERE date = '{dato}'
+        """
+        mycursor.execute(sql)
+        mydb.commit()
+        print(f"Notat lagret for {dato}.")
+            
+
+
+    else:
+        if input("vil du gå tilbake ja/nei") == "ja":
+            start()
+
+def vis_november_2024():
+    text_calendar = calendar.TextCalendar()
+    text_calendar.prmonth(2024, 11)
+    if input("vil du skrive ett notat ja/nei: ").lower() == "ja":
+        dato = valgtDato()
+        # if dato not in range(1, 1000000000000):  
+        #     print("Ugyldig dato!")
+        
+        # else: 
+        notat = input("Skriv notat: ")
+        sql = f"""
+        UPDATE november
+        SET notes = '{notat}'
+        WHERE date = '{dato}'
+        """
+        mycursor.execute(sql)
+        mydb.commit()
+        print(f"Notat lagret for {dato}.")
+            
+
+
+    else:
+        if input("vil du gå tilbake ja/nei") == "ja":
+            start()
+
+
+def vis_desember_2024():
+    text_calendar = calendar.TextCalendar()
+    text_calendar.prmonth(2024, 12)
+    if input("vil du skrive ett notat ja/nei: ").lower() == "ja":
+        dato = valgtDato()
+        # if dato not in range(1, 1000000000000):  
+        #     print("Ugyldig dato!")
+        
+        # else: 
+        notat = input("Skriv notat: ")
+        sql = f"""
+        UPDATE december
+        SET notes = '{notat}'
+        WHERE date = '{dato}'
+        """
+        mycursor.execute(sql)
+        mydb.commit()
+        print(f"Notat lagret for {dato}.")
+            
+
+
+    else:
+        if input("vil du gå tilbake ja/nei") == "ja":
+            start()
 
 def start():
     måned = velg_månede()  
@@ -63,6 +341,26 @@ def start():
             vis_januar_2024()
         elif måned == 2:
             vis_februar_2024()
+        elif måned == 3:
+            vis_mars_2024()
+        elif måned == 4:
+            vis_april_2024()
+        elif måned == 5:
+            vis_mai_2024()
+        elif måned == 6:
+            vis_juni_2024()
+        elif måned == 7:
+            vis_juli_2024()
+        elif måned == 8:
+            vis_august_2024()
+        elif måned == 9:
+            vis_september_2024()
+        elif måned == 10:
+            vis_oktober_2024()
+        elif måned == 11:
+            vis_november_2024()
+        elif måned == 12:
+            vis_desember_2024()
         else:
             print("Funksjon for valgt måned er ikke implementert.")
 
