d12247fae40065f5c35235e367dd4647e265b3de
Milosz <miloszchudzik07@gmail.com>
2024-12-03 14:12:37 +0100

    la til en stats side og oppdaterte sql

---
+++ stats.py
@@ -0,0 +1,9 @@
+import mysql.connector
+
+mydb = mysql.connector.connect(
+    host="10.2.2.119",
+    user="[milosz]",
+    password="[milosz2007]",
+    database="Stats"
+)
+
--- stats.sql
+++ stats.sql
@@ -1,107 +1,86 @@
+-- MySQL Workbench Forward Engineering
 
-CREATE TABLE if not exists øvelser (
-    navn varchar(20),
-    Muskelgruppe varchar(10),
-);
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
 
-INSERT INTO øvelser (navn, Muskelgruppe) VALUES
-    ('benk press', 'chest'),
-    ('incline db', 'chest'),
-    ('chest flies', 'chest'),
-    ('dips', 'chest'),
-    ('pull ups', 'back'),
-    ('lat pulldown', 'back'),
-    ('cable rows', 'back'),
-    ('face pulls', 'back'),
-    ('leg press', 'legs'),
-    ('calf raises', 'legs'),
-    ('leg extensions', 'legs'),
-    ('leg curls', 'legs'),
-    ('deadlift', 'legs'),
-    ('preacher curls', 'biceps'),
-    ('incline curls', 'biceps'),
-    ('tricep dips', 'tricep'),
-    ('tricep extensions', 'tricep'),
-    ('shoulder press', 'skuldre'),
-    ('lat raises', 'skuldre'),
+-- -----------------------------------------------------
+-- Schema Stats
+-- -----------------------------------------------------
+
+-- -----------------------------------------------------
+-- Schema Stats
+-- -----------------------------------------------------
+CREATE SCHEMA IF NOT EXISTS `Stats` DEFAULT CHARACTER SET utf8 ;
+USE `Stats` ;
+
+-- -----------------------------------------------------
+-- Table `Stats`.`bruker`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Stats`.`bruker` (
+  `bruker_id` INT NOT NULL AUTO_INCREMENT,
+  `fornavn` VARCHAR(45) NOT NULL,
+  `etternavn` VARCHAR(45) NOT NULL,
+  `tlf` VARCHAR(45) NOT NULL,
+  `epost` VARCHAR(45) NULL,
+  `passord` VARCHAR(45) NOT NULL,
+  `brukernavn` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`bruker_id`))
+ENGINE = InnoDB;
 
 
+-- -----------------------------------------------------
+-- Table `Stats`.`muskel_gruppe`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Stats`.`muskel_gruppe` (
+  `muskel_gruppe_id` INT NOT NULL AUTO_INCREMENT,
+  `muskel_gruppe` VARCHAR(45) NOT NULL,
+  PRIMARY KEY (`muskel_gruppe_id`))
+ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS chest (
-    navn VARCHAR(20),
-    reps INT,
-    sets INT,
-    weight INT,
-    pr INT
-);
 
-INSERT INTO chest (navn, reps, sets, weight, pr) VALUES 
-    ('benkpress', 6, 3, 100, 120),
-    ('incline dumbell', 8, 3, 30, NULL),
-    ('chest flies (low)', 12, 3, 20, NULL),
-    ('dips', 10, 2, 25, NULL);
+-- -----------------------------------------------------
+-- Table `Stats`.`ovelser`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Stats`.`ovelser` (
+  `ovelse_id` INT NOT NULL AUTO_INCREMENT,
+  `ovelse` VARCHAR(45) NOT NULL,
+  `muskelgruppe` INT NOT NULL,
+  PRIMARY KEY (`ovelse_id`),
+  INDEX `fk_ovelser_muskel_gruppe_idx` (`muskelgruppe` ASC),
+  CONSTRAINT `fk_ovelser_muskel_gruppe`
+    FOREIGN KEY (`muskelgruppe`)
+    REFERENCES `Stats`.`muskel_gruppe` (`muskel_gruppe_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS rygg (
-    navn VARCHAR(20),
-    reps INT,
-    sets INT,
-    weight INT,
-    pr INT
-);
 
-INSERT INTO rygg (navn, reps, sets, weight, pr) VALUES
-    ('pull ups', 10, 2, 5, 13),
-    ('lat pulldown', 10, 3, 65, NULL),
-    ('cable rows', 10, 3, 74, NULL),
-    ('face pulls', 15, 4, 30, NULL);
+-- -----------------------------------------------------
+-- Table `Stats`.`bruker_ovelser`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `Stats`.`bruker_ovelser` (
+  `bruker_id` INT NOT NULL,
+  `ovelse_id` INT NOT NULL,
+  `reps` INT NULL,
+  `sets` INT NULL,
+  `vekt` INT NULL,
+  PRIMARY KEY (`bruker_id`, `ovelse_id`),
+  INDEX `fk_bruker_has_ovelser_ovelser1_idx` (`ovelse_id` ASC),
+  INDEX `fk_bruker_has_ovelser_bruker1_idx` (`bruker_id` ASC),
+  CONSTRAINT `fk_bruker_has_ovelser_bruker1`
+    FOREIGN KEY (`bruker_id`)
+    REFERENCES `Stats`.`bruker` (`bruker_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION,
+  CONSTRAINT `fk_bruker_has_ovelser_ovelser1`
+    FOREIGN KEY (`ovelse_id`)
+    REFERENCES `Stats`.`ovelser` (`ovelse_id`)
+    ON DELETE NO ACTION
+    ON UPDATE NO ACTION)
+ENGINE = InnoDB;
 
-CREATE TABLE IF NOT EXISTS legs (
-    navn VARCHAR(20),
-    reps INT,
-    sets INT,
-    weight INT,
-    pr INT
-);
 
-INSERT INTO legs (navn, reps, sets, weight, pr) VALUES
-    ('squats', 10, 3, 70, NULL),
-    ('calf raises', 10, 3, 30, NULL),
-    ('leg extensions', 8, 2, 70, NULL),
-    ('leg curls', 8, 2, 50, NULL),
-    ('deadlift', 10, 3, 60, NULL);
-
-CREATE TABLE IF NOT EXISTS biceps (
-    navn VARCHAR(20),
-    reps INT,
-    sets INT,
-    weight INT,
-    pr INT
-);
-
-INSERT INTO biceps (navn, reps, sets, weight, pr) VALUES
-    ('preacher curls', 9, 2, 22, NULL),
-    ('incline curls', 24, 3, 12, NULL);
-
-CREATE TABLE IF NOT EXISTS triceps (
-    navn VARCHAR(20),
-    reps INT,
-    sets INT,
-    weight INT,
-    pr INT
-);
-
-INSERT INTO triceps (navn, reps, sets, weight, pr) VALUES
-    ('dips', 10, 2, 25, NULL),
-    ('tricep extensions', 10, 2, 25, NULL);
-
-CREATE TABLE IF NOT EXISTS skuldre (
-    navn VARCHAR(20),
-    reps INT,
-    sets INT,
-    weight INT,
-    pr INT
-);
-
-INSERT INTO skuldre (navn, reps, sets, weight, pr) VALUES
-    ('shoulder press', 10, 3, 28, NULL),
-    ('lateral raises', 15, 2, 12, NULL);
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
